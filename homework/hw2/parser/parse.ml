0cb0b129d912172e5cd71d9460ecfbc330b34747
Hakjoo Oh <hakjoo_oh@korea.ac.kr>
2023-03-14 13:16:23 +0900

    update

--- homework/hw2/parser/parse.ml
+++ homework/hw2/parser/parse.ml
@@ -1,6 +1,83 @@
+open Util
+
 type symbol = T of string | N of string | Epsilon | End
-type production = (symbol * symbol list) list
-type cfg = symbol list * symbol list * symbol * production
+type production = symbol * symbol list
+type cfg = symbol list * symbol list * symbol * production list
+
+let string_of_symbol s = 
+  match s with 
+  | T x -> x 
+  | N x -> x 
+  | Epsilon -> "epsilon"
+  | End -> "$"
+
+let string_of_prod (lhs, rhs) = 
+    string_of_symbol lhs ^ " -> " ^ 
+      string_of_list ~first:"" ~last:"" ~sep:" " string_of_symbol rhs 
+
+module FIRST = struct 
+  type t = (symbol, symbol BatSet.t) BatMap.t
+
+  let empty = BatMap.empty 
+  
+  let find : symbol -> t -> symbol BatSet.t 
+  =fun s t -> try BatMap.find s t with _ -> BatSet.empty 
+  
+  let add : symbol -> symbol -> t -> t 
+  =fun s1 s2 t -> BatMap.add s1 (BatSet.add s2 (find s1 t)) t
+  
+  let add_set : symbol -> symbol BatSet.t -> t -> t 
+  =fun s1 ss t -> BatSet.fold (fun s2 -> add s1 s2) ss t
+
+  let tostring : t -> string
+  =fun t -> 
+    BatMap.foldi (fun s ss str -> 
+      str ^ string_of_symbol s ^ " |-> " ^ string_of_set string_of_symbol ss ^ "\n"
+    ) t ""
+end   
+
+module FOLLOW = struct
+  type t = (symbol, symbol BatSet.t) BatMap.t 
+
+  let empty = BatMap.empty 
+
+  let find : symbol -> t -> symbol BatSet.t 
+  =fun s t -> try BatMap.find s t with _ -> BatSet.empty 
+
+  let add : symbol -> symbol -> t -> t 
+  =fun s1 s2 t -> BatMap.add s1 (BatSet.add s2 (find s1 t)) t
+
+  let add_set : symbol -> symbol BatSet.t -> t -> t 
+  =fun s1 ss t -> BatSet.fold (fun s2 -> add s1 s2) ss t
+
+  let tostring : t -> string
+  =fun t -> 
+    BatMap.foldi (fun s ss str -> 
+      str ^ string_of_symbol s ^ " |-> " ^ string_of_set string_of_symbol ss ^ "\n"
+    ) t ""
+end
+
+module ParsingTable = struct
+  type t = (symbol * symbol, (symbol * symbol list) BatSet.t) BatMap.t 
+
+  let empty = BatMap.empty 
+
+  let find (nonterm, term) t = try BatMap.find (nonterm, term) t with _ -> BatSet.empty 
+
+  let add (nonterm, term) prod t = 
+    BatMap.add (nonterm, term) (BatSet.add prod (find (nonterm, term) t)) t
+    
+  let tostring : t -> string 
+  =fun t -> 
+    BatMap.foldi (fun (nonterm, term) prods str -> 
+      str ^ string_of_symbol nonterm ^ ", " ^ string_of_symbol term ^ " |-> " ^
+        string_of_set string_of_prod prods ^ "\n"
+    ) t ""
+    
+  let foldi = BatMap.foldi 
+
+  let for_all = BatMap.for_all
+end
 
 let check_LL1 : cfg -> bool
 =fun _ -> false (* TODO *)
