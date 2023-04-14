b199de060d94c2cec6e56f0a25354d7947c7ae6e
Hakjoo Oh <hakjoo_oh@korea.ac.kr>
2023-04-14 14:48:08 +0900

    update

--- homework/bootstrap.sh
+++ homework/bootstrap.sh
@@ -48,7 +48,7 @@ echo "[NOTE] End-up Initialize OPAM"
 # Setup OCAML Dependencies
 echo "[NOTE] Start Setup OCAML Dependencies"
 eval $(opam env)
-for pkg in "batteries 3.5.1" "core 0.15.0" "dune 3.5.0" "menhir 20200624" "zarith 1.10" "logs 0.7.0" "mtime 1.2.0" "yojson 2.0.2"; do
+for pkg in "batteries 3.5.1" "core 0.15.0" "dune 3.5.0" "menhir 20200624" "zarith 1.10" "logs 0.7.0" "mtime 1.2.0" "yojson 2.0.2" "ocamlgraph 2.0.0"; do
   pkg_pair=( $pkg )
   pkg_name=${pkg_pair[0]}
   pkg_version=${pkg_pair[1]}
